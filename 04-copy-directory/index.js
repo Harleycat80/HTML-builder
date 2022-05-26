7a2e43a184d02fddb923bb1f0fc41a593ccf9b95
Harleycat80 <Harleycat@rambler.ru>
2022-05-26 17:06:15 +0400

    fix: add deleting files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,11 +5,26 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true },(err)=>{
 if(err){
 
 throw (err)}
+fs.readdir(path.join(__dirname, 'files-copy'), (err, data)=>{
+   if(err){
+      throw err
+   }
+   console.log(data);
+   data.forEach(item => fs.unlink(path.join(__dirname, 'files-copy'+'/'+item), (err)=>{
+      if(err){
+         throw err
+      }
+   })
+   )
+   
+})
 
 fs.readdir(path.join(__dirname, 'files'),function(err,data){
    if (err){
       console.error(err)
    }
+
+   
 data.forEach(file=>{fs.promises.copyFile(path.join(__dirname, 'files')+'/'+file,path.join(__dirname, 'files-copy')+'/'+file )})
 })
 });
